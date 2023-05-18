4d3b95163b488c39bb1af45ccf0c9e727d025f64
SadmanJawad <jawadnabil977@gmail.com>
2023-05-18 12:00:37 +0600

    navbar + login route implemented

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,10 @@
+import error from '../../assets/error.jpg'
+const Error = () => {
+    return (
+        <div>
+            <img className='h-screen mx-auto' src={error} alt="" />
+        </div>
+    );
+};
+
+export default Error;
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,23 +1,29 @@
+import { Link } from 'react-router-dom';
+import companyLogo from '../../../assets/logo.png'
 
 const Navbar = () => {
     return (
       <div>
-<nav className="bg-white border-gray-200 dark:bg-gray-900">
+<nav className="bg-sky-300 border-gray-200 dark:bg-gray-900">
     <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-        <a href="https://flowbite.com" className="flex items-center">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        </a>
+        <Link to='/' className="flex items-center">
+            <img src={companyLogo} className="w-16 h-16" alt="Logo" />
+            <span className="self-center text-3xl mt-1 ml-2 font-bold whitespace-nowrap text-white dark:text-white">Toytopia</span>
+        </Link>
+    <div>
+      <input type="text" placeholder="Search" className="input input-bordered" />
+       </div>
         <div className="flex items-center">
-            <a href="tel:5541251234" className="mr-6 text-sm  text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
-            <a href="#" className="text-sm  text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+         <p href="tel:5541251234" className="mr-2 text-md  text-white dark:text-white">Welcome to Toytopia</p>
+            <span className='mr-2 ml-2 text-white'>|</span>
+        <Link to='/login' className="ml-2 text-md  text-white dark:text-blue-500 hover:underline">Login</Link>
         </div>
     </div>
 </nav>
 <nav className="bg-gray-50 dark:bg-gray-700">
     <div className="max-w-screen-xl px-4 py-3 mx-auto">
         <div className="flex items-center">
-            <ul className="flex flex-row font-medium mt-0 mr-6 space-x-8 text-sm">
+            <ul className="flex flex-row font-extralight mt-0 mr-6 space-x-8 text-md ml-96">
                 <li>
                     <a href="#" className="text-gray-900 dark:text-white hover:underline" aria-current="page">Home</a>
                 </li>
@@ -29,6 +35,9 @@ const Navbar = () => {
                 </li>
                 <li>
                     <a href="#" className="text-gray-900 dark:text-white hover:underline">Features</a>
+                </li>
+                <li>
+                    <a href="#" className="text-gray-900 dark:text-white hover:underline">Contact Us</a>
                 </li>
             </ul>
         </div>
