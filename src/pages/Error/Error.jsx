24bf98d4250b49ae92bcb6fc43f616c0ec8c26d5
SadmanJawad <jawadnabil977@gmail.com>
2023-05-20 00:01:54 +0600

    login route+page implemented

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,7 +1,8 @@
-import error from '../../assets/error.jpg'
+import error from '../../assets/404.jpg'
 const Error = () => {
     return (
         <div>
+            
             <img className='h-screen mx-auto' src={error} alt="" />
         </div>
     );
