88e4c2dce27adfd8d86a3cb80bc536edee401404
SadmanJawad <jawadnabil977@gmail.com>
2023-05-20 00:25:52 +0600

    create Auth context setup && signUp

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,42 +1,78 @@
 import { Link } from "react-router-dom";
-import img from '../../assets/images/img.svg'
+import img from "../../assets/images/img.svg";
+import { FaGoogle } from "react-icons/fa";
 
 const Login = () => {
-    return (
-        <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row">
-            <div className="w-1/2 mr-12">
-                <img src={img} alt="" />
-            </div>
-            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                <div className="card-body">
-                    <h1 className="text-4xl text-center font-thin">Login</h1>
-                    <form>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <input className="btn bg-sky-500 border-none" type="submit" value="Login" />
-                        </div>
-                    </form>
-                    <p className='my-4 text-center text-sm'>New to Toytopia <Link className='text-sky-700 underline font-normal' to="/signup">Sign Up</Link> </p>
-                </div>
-            </div>
+
+    const handleLogin = event => {
+        event.preventDefault(); // prevent page from reloading on submit
+        console.log('login form submitted');
+    }
+
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row">
+        <div className="w-1/2 mr-12">
+          <img src={img} alt="" />
         </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card-body">
+            <h1 className="text-4xl text-center font-thin">Login</h1>
+            <form onSubmit={handleLogin}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="text"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  className="btn bg-sky-500 border-none"
+                  type="submit"
+                  value="Login"
+                />
+              </div>
+            </form>
+            <p className="my-4 text-center text-sm">
+              New to Toytopia{" "}
+              <Link className="text-sky-700 underline font-normal" to="/signup">
+                Sign Up
+              </Link>{" "}
+            </p>
+            {/* google button */}
+            <button
+              type="button"
+              className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-full text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
+            >
+              <FaGoogle className="mr-2" />
+              Sign in with Google
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-);
+  );
 };
 
 export default Login;
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,64 @@
+import { Link } from "react-router-dom";
+import img from "../../assets/images/img.svg";
+import { FaGoogle } from "react-icons/fa";
+
 const SignUp = () => {
+
+    const handleSignUp = event => {
+        event.preventDefault(); // prevent page from reloading on submit  
+        console.log('sign up form submitted');
+    }
+    
+
     return (
-        <div>
-    <h2 className="text-7xl">Sign Up Here</h2>            
+        <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+            <div className="w-1/2 mr-12">
+                <img src={img} alt="" />
+            </div>
+            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <div className="card-body">
+                    <h1 className="text-3xl text-center font-bold">Sign Up</h1>
+                    <form onSubmit={handleSignUp}>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name='name' placeholder="name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Confirm Password</span>
+                            </label>
+                            <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn bg-sky-500 border-none" type="submit" value="Sign Up" />
+                        </div>
+                    </form>
+                    <p className='my-4 text-center text-sm'>Already Have an Account? <Link className='text-sky-700 underline font-normal' to="/login">Login</Link> </p>
+               {/* google button */}
+            <button
+              type="button"
+              className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-full text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
+            >
+              <FaGoogle className="mr-2" />
+              Sign in with Google
+            </button>
+                </div>
+            </div>
         </div>
-    );
+    </div>
+);
 };
 
 export default SignUp;
